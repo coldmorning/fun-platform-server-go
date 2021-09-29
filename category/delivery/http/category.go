236898a4ad56801235c342ebec6a7646a3913419
Huang coldmorning <coldmorning360@gmail.com>
2021-09-29 23:56:08 +0800

    feat (category: controller): Add  category delivery  to project.

---
+++ category/delivery/http/category.go
@@ -0,0 +1,22 @@
+package http
+
+import (
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func List(c *gin.Context){
+}
+
+func Create(c *gin.Context){
+}
+
+func Delete(C *gin.Context){
+}
+
+
+
+func Update(c *gin.Context){
+}

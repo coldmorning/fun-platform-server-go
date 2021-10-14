783dadf26f1764d9180b6026d32bd7157536b200
Huang coldmorning <coldmorning360@gmail.com>
2021-10-14 20:12:34 +0800

    fix(env_config): Read another path.

--- config/env_config.go
+++ config/env_config.go
@@ -1,32 +1,33 @@
 package config
 
-import(
-	"os"
+import (
 	"log"
-	"github.com/spf13/viper"
+	"os"
 
+	"github.com/spf13/viper"
 )
-func GetConfig()( *viper.Viper,error){
-	path,err := os.Getwd()
-	
+
+func GetConfig() (*viper.Viper, error) {
+	path, err := os.Getwd()
+
 	if err != nil {
 		log.Println("read path error")
 	}
-	confi_path := path[:len(path)-4] +"/config"; 
-	
-    config := viper.New()
-	config.AddConfigPath(confi_path) 
+	confi_path := path + "\\config"
+
+	config := viper.New()
+	config.AddConfigPath(confi_path)
 	config.SetConfigType("yaml")
-    config.SetConfigName ("dev-env")
+	config.SetConfigName("dev-env")
 	if err := config.ReadInConfig(); err != nil {
-        if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-            // Config file not found; ignore error if desired
-            log.Println("no such config file")
-        } else {
-            // Config file was found but another error was produced
-            log.Println("read config error")
-        }
-        //log.Fatal(err) // failed to read configuration file. Fatal error
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Println("no such config file")
+		} else {
+			// Config file was found but another error was produced
+			log.Println("read config error")
+		}
+		//log.Fatal(err) // failed to read configuration file. Fatal error
 	}
 	return config, err
 }

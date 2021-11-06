dd5539eef4119d366b2f75580950e225b2501d0a
Huang coldmorning <coldmorning360@gmail.com>
2021-11-06 21:26:10 +0800

    feat (auth: service_test):  Add the test function to CreateToken / CreateRefreshtoken func.

--- auth/service/auth_service_test.go
+++ auth/service/auth_service_test.go
@@ -1,19 +1,43 @@
 package authservice
 
 import (
-
     "testing"
 	"github.com/coldmorning/fun-platform/model"
 	"github.com/stretchr/testify/assert"
-
 )
 
 
+
+
+
+
+func TestCreateToken(t *testing.T){
+	td := &model.TokenDetails{}
+	userId := "15e3eae4-20a1-40a7-bb7e-380dbf091357"
+	td,_ = CreateAccessToken(userId,td)
+	td,_ = CreateRefreshToken(userId,td)
+	assert.NotNil(t, td)
+	assert.NotNil(t, td.AccessToken)
+	assert.NotNil(t, td.RefreshToken)
+
+
+}
+
 func TestCreateAccessToken(t *testing.T){
 	td := &model.TokenDetails{}
-	userId := "15e3eae4-20a1-40f7-bb7e-380dbf091356"
-	td, err := authservice.CreateAccessToken(userId,td)
+	userId := "15e3eae4-20a1-40a7-bb7e-380dbf091357"
+	td,_ = CreateAccessToken(userId,td)
 	assert.NotNil(t, td)
-	assert.Equal(t, 36,len( td.AccessToken))
-	
+	assert.NotNil(t, td.AccessToken)
+
+
+}
+
+func TestCreateRefreshToken(t *testing.T){
+	td := &model.TokenDetails{}
+	userId := "15e3eae4-20a1-40a7-bb7e-380dbf091357"
+	td,_ = CreateRefreshToken(userId,td)
+	assert.NotNil(t, td)
+	assert.NotNil(t, td.RefreshToken)
+
 }

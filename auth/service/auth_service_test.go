b469e6f4736f5f27396195d9338d9eee24a4ca68
Huang coldmorning <coldmorning360@gmail.com>
2021-11-10 23:30:29 +0800

    feat(auth:service_test): Add Unit test to auth_service_test ( TestVerifyTokenOk  & TestVerifyTokenErrorExpired  method).

--- auth/service/auth_service_test.go
+++ auth/service/auth_service_test.go
@@ -1,13 +1,13 @@
 package authservice
 
 import (
+
     "testing"
+	"time"
 	"github.com/coldmorning/fun-platform/model"
 	"github.com/stretchr/testify/assert"
+	"github.com/dgrijalva/jwt-go"
 )
-
-
-
 
 
 
@@ -39,5 +39,38 @@ func TestCreateRefreshToken(t *testing.T){
 	td,_ = CreateRefreshToken(userId,td)
 	assert.NotNil(t, td)
 	assert.NotNil(t, td.RefreshToken)
+}
+
+func TestVerifyTokenOk(t *testing.T) {
+	td := &model.TokenDetails{}
+	userId := "15e3eae4-20a1-40a7-bb7e-380dbf091357"
+	td,_ = CreateAccessToken(userId,td)
+	token, _:= VerifyToken(Access_secret,td.AccessToken)
+	assert.NotNil(t, token)
+}
+
+
+func TestVerifyTokenErrorExpired(t *testing.T) {
+		//Create expect token
+		var err error
+		td := &model.TokenDetails{}
+		td.AtExpires = time.Now().Add( -1 * time.Second).Unix()
+		atClaims := jwt.MapClaims{}
+		atClaims["exp"] = td.AtExpires
+		at := jwt.NewWithClaims(jwt.SigningMethodHS512, atClaims)
+		td.AccessToken, err = at.SignedString(Access_secret)
+		if err == nil {
+			_, errToken := VerifyToken(Access_secret,td.AccessToken)
+			  // assert for not nil (good when you expect something)
+			  
+			  if assert.NotNil(t,errToken) {
+				// now we know that object isn't nil, we are safe to make
+				// further assertions without causing any errors
+				assert.Equal(t,ErrExpiredToken.Error(), errToken.Error())
+			  }
+		}
+}
+
+func TestVerifyTokenErrorFormat(t *testing.T) {
 
 }
